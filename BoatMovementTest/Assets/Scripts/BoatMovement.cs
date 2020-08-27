b8a683e27c12a6932d2a748ee7bc8a52070d09da
Flargy <marlun1996@hotmail.com>
2020-08-27 14:44:00 +0200

    minor fixes

--- BoatMovementTest/Assets/Scripts/BoatMovement.cs
+++ BoatMovementTest/Assets/Scripts/BoatMovement.cs
@@ -4,15 +4,109 @@ using UnityEngine;
 
 public class BoatMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float rotationSpeed = 10;
+    [SerializeField] Transform meshTransform;
+
+    private Vector3 lookDirection = Vector3.zero;
+    private float rotationAmount = 0f;
+
+    private Quaternion startRotationForMesh;
+    [SerializeField] private Vector3 shipRotationValue;
+
+    private Coroutine rotationRoutine;
+    private Quaternion shipRotation;
+    private Quaternion currentRotationTargetValue;
+    private bool coroutineRunning;
+
     void Start()
     {
-        
+        startRotationForMesh = meshTransform.rotation;
+        shipRotation = Quaternion.Euler(shipRotationValue);
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()   
     {
+        rotationAmount += Time.deltaTime * 0.05f;
+       
+        FaceTowardsDirection();
+        transform.position += transform.forward * rotationSpeed * Time.deltaTime;
 
     }
+
+    /// <summary>
+    /// Thanks Hjalmar for being so awesome with your knowledge about code. I owe you my life.
+    /// </summary>
+    private void FaceTowardsDirection()
+    {
+        lookDirection = transform.position + WindManager.GetWindDirection();
+
+        float angle = Vector3.SignedAngle(transform.forward, WindManager.GetWindDirection(), Vector3.up);
+
+        if(angle > 5 || angle < -5)
+        {
+            RotateMyShip(angle);
+        }
+        else
+        {
+            if (coroutineRunning == true && transform.rotation != Quaternion.identity)
+            {
+                StopCoroutine(rotationRoutine);
+                rotationRoutine = StartCoroutine(RotateMyMesh(Quaternion.identity));
+            }
+        }
+    }
+
+
+    void RotateMyShip(float angle)
+    {
+
+        if (angle > 0)
+        {
+            
+            transform.RotateAround(transform.position, Vector3.up, WindManager.GetWindStrength() * Time.deltaTime);
+            if (coroutineRunning == true && currentRotationTargetValue != shipRotation)
+            {
+                StopCoroutine(rotationRoutine);
+                rotationRoutine = StartCoroutine(RotateMyMesh(shipRotation));
+                currentRotationTargetValue = shipRotation;
+            }
+            else if(coroutineRunning == false && currentRotationTargetValue != transform.rotation)
+            {
+                rotationRoutine = StartCoroutine(RotateMyMesh(shipRotation));
+                currentRotationTargetValue = shipRotation;
+            }
+        }
+        else
+        {
+            transform.RotateAround(transform.position, Vector3.up, -WindManager.GetWindStrength() * Time.deltaTime);
+            if (coroutineRunning == true && currentRotationTargetValue != Quaternion.Inverse(shipRotation))
+            {
+                StopCoroutine(rotationRoutine);
+                rotationRoutine = StartCoroutine(RotateMyMesh(Quaternion.Inverse(shipRotation)));
+            }
+            else if (coroutineRunning == false && transform.rotation != currentRotationTargetValue)
+            {
+                rotationRoutine = StartCoroutine(RotateMyMesh(Quaternion.Inverse(shipRotation)));
+            }
+
+        }
+    }
+
+    private IEnumerator RotateMyMesh(Quaternion newRotation)
+    {
+        coroutineRunning = true;
+        Quaternion currentRotation = meshTransform.rotation;
+        float timer = 0f;
+        float duration = 5f;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            meshTransform.rotation = Quaternion.Slerp(currentRotation, newRotation, timer / duration);
+            yield return null;
+        }
+        coroutineRunning = false;
+        meshTransform.rotation = newRotation;
+    }
+   
 }
--- BoatMovementTest/Assets/Scripts/WindManager.cs
+++ BoatMovementTest/Assets/Scripts/WindManager.cs
@@ -6,9 +6,11 @@ public class WindManager : MonoBehaviour
 {
     private static WindManager instance;
 
-    private float windStrenght = 0;
+    private float windStrenght = 2f;
     private float windDuration = 0;
     private Vector3 windDirection = Vector3.zero;
+    private float min = -1;
+    private float max = 1;
 
     void Start()
     {
@@ -17,16 +19,39 @@ public class WindManager : MonoBehaviour
             instance = this;
         }
         windDirection = Vector3.forward;
+        StartNewWind();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartNewWind();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void StartNewWind()
     {
-        
+        Vector3 newWindDirection = windDirection * -1;
+
+        while (Vector3.Dot(windDirection, newWindDirection) < 0.78f)
+        {
+            newWindDirection = new Vector3(Random.Range(min, max), 0, Random.Range(min, max));
+        }
+        windDirection = newWindDirection.normalized;
+
     }
 
     public static Vector3 GetWindDirection()
     {
         return instance.windDirection;
+    }
+
+    public static float GetWindStrength()
+    {
+
+        return instance.windStrenght;
     }
 }
