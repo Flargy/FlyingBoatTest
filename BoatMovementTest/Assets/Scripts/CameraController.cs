ccda5d77cc0bf17aeb0e284b2accaa323a275c80
Flargy <marlun1996@hotmail.com>
2020-08-27 11:01:55 +0200

    Project created

---
+++ BoatMovementTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject targetToFollow = null;
+    [SerializeField] private Vector3 cameraOffset = Vector3.zero;
+    [SerializeField] private float cameraMovementSPeed = 1.0f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BoatMovementTest/Assets/Scripts/WindManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindManager : MonoBehaviour
+{
+    private static WindManager instance;
+
+    private float windStrenght = 0;
+    private float windDuration = 0;
+    private Vector3 windDirection = Vector3.zero;
+
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        windDirection = Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static Vector3 GetWindDirection()
+    {
+        return instance.windDirection;
+    }
+}
