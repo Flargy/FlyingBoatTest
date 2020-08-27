a19bccaf97bf6fa324b6b65c28bae0cb6fd89708
Flargy <marlun1996@hotmail.com>
2020-08-27 15:56:22 +0200

    wind system updated

--- BoatMovementTest/Assets/Scripts/WindManager.cs
+++ BoatMovementTest/Assets/Scripts/WindManager.cs
@@ -6,11 +6,28 @@ public class WindManager : MonoBehaviour
 {
     private static WindManager instance;
 
+    [SerializeField] private float minimumWindDuration = 10f;
+    [SerializeField] private float maximumWindDuration = 60f;
+
+    [SerializeField] private float minimumCalmWindDuration = 30f;
+    [SerializeField] private float maximumCalmWIndDuration = 120f;
+    [Range(0, 100)]
+    [SerializeField] private int oddsOfNoWind = 0;
+
+    [SerializeField] private float minimumStormDuration = 30f;
+    [SerializeField] private float maximumStormDuration = 100f;
+    [Range(0, 100)]
+    [SerializeField] private int oddsOfStorm = 0;
+    [Range(40, 160)]
+    [SerializeField] private int stormCooldown = 100;
+
     private float windStrenght = 2f;
     private float windDuration = 0;
     private Vector3 windDirection = Vector3.zero;
     private float min = -1;
     private float max = 1;
+    private bool stormAllowed = true;
+    private bool stormActive = false;
 
     void Start()
     {
@@ -19,7 +36,7 @@ public class WindManager : MonoBehaviour
             instance = this;
         }
         windDirection = Vector3.forward;
-        StartNewWind();
+        StartCoroutine(WeatherSwapDelay());
 
     }
 
@@ -34,14 +51,45 @@ public class WindManager : MonoBehaviour
     // Update is called once per frame
     private void StartNewWind()
     {
+        if(StopWinds() == true || StartStorm())
+        {
+            return;
+        }
+       
+        windDuration = Random.Range(minimumWindDuration, maximumWindDuration);
         Vector3 newWindDirection = windDirection * -1;
 
         while (Vector3.Dot(windDirection, newWindDirection) < 0.78f)
         {
             newWindDirection = new Vector3(Random.Range(min, max), 0, Random.Range(min, max));
         }
+
         windDirection = newWindDirection.normalized;
 
+    }
+
+    private bool StartStorm()
+    {
+        if(stormAllowed == false || Random.Range(minimumStormDuration, maximumStormDuration) > oddsOfStorm)
+        {
+            return false;
+        }
+        //start wind
+
+        return true;
+
+    }
+
+    private bool StopWinds()
+    {
+        if (Random.Range(0, 100) < oddsOfNoWind)
+        {
+            Debug.Log("no wind");
+            windDirection = Vector3.zero;
+            windDuration = Random.Range(minimumCalmWindDuration, maximumCalmWIndDuration);
+            return true;
+        }
+        return false;
     }
 
     public static Vector3 GetWindDirection()
@@ -53,5 +101,15 @@ public class WindManager : MonoBehaviour
     {
 
         return instance.windStrenght;
+    }
+
+
+    private IEnumerator WeatherSwapDelay()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(windDuration);
+            StartNewWind();
+        }
     }
 }
